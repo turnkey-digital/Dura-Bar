be5569e15d56b2dfac8b33cfe83d04b26b5d51be
TKD-Jon <jknoll@turnkeydigital.comm>
2019-03-04 16:04:10 -0600

    Added the sidebar functionality

--- js/side-bar.js
+++ js/side-bar.js
@@ -1,2 +1,41 @@
 // This File is just for development. 
 // The code for this is inside applicationCache.js starting on line 55
+
+updateActiveHeight();
+
+$('.close-menu').click(function(){
+    // Declare Vars
+    var menuNumber = $(this).attr("menu");
+    // Call Functions
+    closeOtherMenus(menuNumber);
+    checkThisMenu(menuNumber);
+});
+
+function closeOtherMenus(thisMenu) {
+    var totalMenus = $('.side-bar-sub-menu > div').length;
+    for (i = 1; i <= totalMenus; i++) {
+        if (i == thisMenu) {
+            null
+        } else {
+            closeThisMenu(i)
+        }
+    }
+}
+function checkThisMenu(menuNumber) {
+    var thisMenu = '.menu-' + menuNumber;
+    if ($(thisMenu).hasClass("active")) {
+        $(thisMenu).removeClass('active');
+        $(thisMenu).parent('div').css('height', 0)
+    }  else {
+        $(thisMenu).addClass('active');
+        updateActiveHeight();
+    }
+}
+function closeThisMenu(props) {
+    var thisMenu = '.menu-'+props
+    $(thisMenu).removeClass('active')
+    $(thisMenu).parent('div').css('height', 0)
+}
+function updateActiveHeight() {
+    $('.active').parent('div').css('height', $('.active').height())
+}
