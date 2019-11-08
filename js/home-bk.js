012dfb49163f001dd27bad512f84a5b83ff7785c
TKD-Jon <jknoll@turnkeydigital.com>
2019-11-08 14:35:02 -0600

    Updated
    Final

--- js/home-bk.js
+++ js/home-bk.js
@@ -16,10 +16,8 @@ let latAndLon = {
         "lat": 42.289313,
         "lon": -88.413269,
         "name": "Woodstock",
-        "gmURL": "https://goo.gl/maps/BUGMZKy6LHuo4ik26",
-        "address1": "2195 West Lake Shore Drive",
-        "address2": "Woodstock, Illinois 60098",
-        "phone": "888.387.2227",
+        "address": "Woodstock, Illinois 2195 W.Lake Shore Drive",
+        "phone": "888.DURA.BAR",
         "fax": "815.338.4608",
         "desc": "Conveniently located 50 miles northwest of Chicago, this location carries a large inventory of Dura-Bar and Bronze material in rounds, rectangles, squares and tubes available for as soon as same day shipment. Services include cutting, drilling, milling and turning.",
         "url": "nearest-location.jpg"
@@ -28,21 +26,17 @@ let latAndLon = {
         "lat": 35.631888,
         "lon": -80.531930,
         "name": "Carolina",
-        "gmURL": "https://goo.gl/maps/mTBaEEU3ptEr8D917",
-        "address1": "770 Cedar Springs Road",
-        "address2": "Salisbury, North Carolina 28147",
+        "address": "770 Cedar Springs Road Salisbury, North Carolina 28147",
         "phone": "888.387.2227",
         "fax": "704.637.9736",
         "desc": "Salisbury is a city in the Piedmont region of North Carolina; it has been the county seat of Rowan County since 1753 when Rowan County was much larger and its territory extended to the Mississippi",
         "url": "nc.jpeg"
     },
-    2: { 
+    2: {
         "lat": 40.041201,
         "lon": -76.738538,
         "name": "Pennsylvania",
-        "gmURL": "https://goo.gl/maps/5mmHf1XpZwXkZezJ7",
-        "address1": "90 Grumbacher Road",
-        "address2": "York, Pennsylvania 17404",
+        "address": "90 Grumbacher Road York, Pennsylvania 17404",
         "phone": "888.387.2227",
         "fax": "973.589.3645",
         "desc": "York is a city in southern Pennsylvania. Downtown, the Historical Society Museum displays fine art, furniture and tall-case clocks. Nearby is the Victorian-era Bonham House. The Agricultural and Industrial Museum explores the city’s farming and manufacturing past. The Colonial Complex includes a mid-1700s home, a courthouse and the Golden Plough Tavern. The area’s vineyards are part of the Mason-Dixon Wine Trail.",
@@ -52,9 +46,7 @@ let latAndLon = {
         "lat": 32.793156,
         "lon": -97.248036,
         "name": "Texas",
-        "gmURL": "https://goo.gl/maps/ytNMLUEJUbvqXNQz8",
-        "address1": "2320 Pecan Court",
-        "address2": "Fort Worth, Texas 76117-5336",
+        "address": "2320 Pecan Court Fort Worth, Texas 76117-5336",
         "phone": "817.831.6306",
         "fax": "817.831.7220",
         "desc": "Fort Worth is a city in North Central Texas. In the late 19th century, it became an important trading post for cowboys at the end of the Chisholm Trail. Today, it's a modern city, with international art institutions like the Kimbell Art Museum. The Fort Worth Stockyards are home to rodeos, and the National Cowgirl Museum and Hall of Fame honors pioneers.",
@@ -70,32 +62,18 @@ let distances = [null, null, null, null]
 // All Functions
 function geoLocation() {
     navigator.geolocation.getCurrentPosition(position);
-    
-    navigator.permissions.query({name:'geolocation'}).then(function(result) {
-    // Will return ['granted', 'prompt', 'denied']
-    console.log(result.state);
-    (result.state == 'prompt' ? promtAgain() : result.state == 'denied' ? $('.homePageLazyLoader').addClass('hidden') : null)
-    });
 };
-
-function promtAgain() {
-    setTimeout(function () {
-        geoLocation();
-    }, 3000);
-
-}
 
 function position(position) {
     let lat1 = position.coords.latitude;
     let lon1 = position.coords.longitude;
     console.table(position);
-    
+
     checkCountry(lat1, lon1, areYouInTheUSA);
-    
+
 }
 
 function inUSA(lat1, lon1) {
-
     for (i = 0; i < Object.keys(latAndLon).length; i++) {
         var thisLat = latAndLon[i].lat;
         var thisLon = latAndLon[i].lon;
@@ -105,9 +83,7 @@ function inUSA(lat1, lon1) {
         distances[i] = {
             "name": latAndLon[i].name,
             "distance": totalDistance,
-            "gmURL": latAndLon[i].gmURL,
-            "address1": latAndLon[i].address1,
-            "address2": latAndLon[i].address2,
+            "address": latAndLon[i].address,
             "phone": latAndLon[i].phone,
             "fax": latAndLon[i].fax,
             "desc": latAndLon[i].desc,
@@ -116,28 +92,24 @@ function inUSA(lat1, lon1) {
     }
 
 
-    var currentOption = {
+    var currentLocation = {
         "name": "Woodstock",
         "distance": 500,
-        "gmURL": "https://goo.gl/maps/BUGMZKy6LHuo4ik26",
-        "address1": "2195 West Lake Shore Drive,",
-        "address2": "Woodstock, Illinois 60098",
-        "phone": "888.387.2227",
+        "address": "Woodstock, Illinois 2195 W.Lake Shore Drive",
+        "phone": "888-DURA-BAR",
         "fax": "815.338.4608",
         "desc": "Conveniently located 50 miles northwest of Chicago, this location carries a large inventory of Dura-Bar and Bronze material in rounds, rectangles, squares and tubes available for as soon as same day shipment. Services include cutting, drilling, milling and turning.",
         "url": "nearest-location.jpg"
     }
 
     for (i = 0; i < Object.keys(distances).length; i++) {
-        (currentOption.distance > distances[i].distance ? updateCurrentOption(distances[i]) : null)
+        (currentLocation.distance > distances[i].distance ? updatecurrentLocation(distances[i]) : null)
 
-        function updateCurrentOption(update) {
-            currentOption = {
+        function updateCurrentLocation(update) {
+            currentLocation = {
                 "name": update.name,
                 "distance": update.distance,
-                "gmURL": update.gmURL,
-                "address1": update.address1,
-                "address2": update.address2,
+                "address": update.address,
                 "phone": update.phone,
                 "fax": update.fax,
                 "desc": update.desc,
@@ -146,16 +118,8 @@ function inUSA(lat1, lon1) {
         }
     }
 
-    console.table(currentOption);
-    $('#gmURL').attr('href', currentOption.gmURL);
-    $('#address1').text(currentOption.address1);
-    $('#address2').text(currentOption.address2);
-    $('#phoneURL').attr('href', `tel:+1.${currentOption.phone}`);
-    $('#phone').text(currentOption.phone);
-    $('#fax').text(currentOption.fax);
-    $('#desc').text(currentOption.desc);
-    $('div.nearest-location').css('background', `url("../images/home/${currentOption.url}") no-repeat center center / cover`);
-    $('.homePageLazyLoader').addClass('hidden')
+    console.log(currentLocation)
+    updateSection(currentLocation);
 
 }
 
@@ -167,7 +131,7 @@ function checkCountry(lat, lon, bool) {
         })
         .then(data => {
             (data.countryCode == 'US' ? bool = !bool : null);
-            (bool ? $('.homePageLazyLoader').addClass('hidden') : inUSA(lat, lon));
+            (bool ? console.log('You have either denied the browser permissions to access your location. Or you are outside of the United States of America') : inUSA(lat, lon));
         })
         .catch(err => {
             // Do something for an error here
@@ -178,9 +142,7 @@ function checkCountry(lat, lon, bool) {
 function distance(lat1, lon1, lat2, lon2, unit) {
     if ((lat1 == lat2) && (lon1 == lon2)) {
         return 0;
-    }
-
-    else {
+    } else {
         var radlat1 = Math.PI * lat1 / 180;
         var radlat2 = Math.PI * lat2 / 180;
         var theta = lon1 - lon2;
@@ -192,7 +154,14 @@ function distance(lat1, lon1, lat2, lon2, unit) {
         dist = Math.acos(dist);
         dist = dist * 180 / Math.PI;
         dist = dist * 60 * 1.1515;
-        if (unit == "K") { dist = dist * 1.609344 }
+        if (unit == "K") {
+            dist = dist * 1.609344
+        }
         return dist;
     }
+}
+
+function updateSection(location) {
+    $('#address').html(location.address);
+    $('#address').html('test');
 }
